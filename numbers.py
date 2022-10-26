ce08a5bbd1ccc7cffeb464eaea3e9a302dfae9b2
John Kinyua <kingjohnkn@gmail.com>
2022-10-26 19:17:11 +0300

    lab4

--- numbers.py
+++ numbers.py
@@ -12,5 +12,37 @@ def numbers():
     myfile.close()
     print("Finished writing the File")
 
+def read_numbers():
+    print()
+    infile = open("numbers.txt", 'r')
+    # allstr = infile.read()
+    # print(allstr)
+
+    for i in range(10):
+        s = infile.readline()
+        # remove trailing newline character
+        print(s[:-1])
+
+    infile.close()
+
+def read_numbers2():
+    infile = open("numbers.txt", 'r')
+
+    # lines = infile.readlines()
+    # --- Print the lines in the file in a list ---
+    # print(lines)
+
+    # --- Read the file as a list of lines ---
+    # for s in lines:
+    #     print(s[:-1])
+
+    # --- Use the file as if it is a list of lines ---
+    for s in infile:
+        print(s[:-1])
+        
+    infile.close()
+
 
 numbers()
+read_numbers()
+read_numbers2()
